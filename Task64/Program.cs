82098280eb4c89f055da1256f0a3c93e4e1cc76e
Nastya224 <94219446+Nastya224@users.noreply.github.com>
2022-12-17 18:56:45 +0300

    Add files via upload

---
+++ Task64/Program.cs
@@ -0,0 +1,12 @@
+﻿
+Console.WriteLine("Введите натуральное число больше 0:");
+int N = int.Parse(Console.ReadLine());
+ 
+void NCounter (int N)
+{
+    if (N == 0) return;
+    Console.Write("{0,4}", N);
+    NCounter (N - 1);
+}
+ 
+NCounter(N);
